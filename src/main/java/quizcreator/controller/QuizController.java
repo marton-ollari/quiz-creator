91bb66a30632ef01f8f5beb9eb302a837fe57298
Ollári Márton <ollarimarton@gmail.com>
2018-04-04 13:32:58 +0200

    first simple route

--- src/main/java/quizcreator/controller/QuizController.java
+++ src/main/java/quizcreator/controller/QuizController.java
@@ -2,6 +2,8 @@ package quizcreator.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import quizcreator.service.QuizService;
 
 @Controller
@@ -9,4 +11,9 @@ public class QuizController {
 
     @Autowired
     private QuizService quizService;
+
+    @RequestMapping(name = "/", method = RequestMethod.GET)
+    public String mainPage(){
+        return "index";
+    }
 }
