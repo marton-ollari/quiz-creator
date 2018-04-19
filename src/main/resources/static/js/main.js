391171b33daf66d626c972f72747c64dc2538243
Ollári Márton <ollarimarton@gmail.com>
2018-04-19 13:54:38 +0200

    create quiz modal

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
 });
 
 function addEventListeners() {
+    createQuizEventListener();
     $.getJSON("/islogged", function (isLogged) {
         if (isLogged === "false"){
             $(".login-modal-button").removeClass("hidden");
@@ -11,27 +12,30 @@ function addEventListeners() {
             $(".create-quiz-button").removeClass("hidden");
             $(".play-button").removeClass("hidden");
             $(".logout-button").removeClass("hidden");
-            createQuizEventListener();
         }
     })
 }
 
 function createQuizEventListener() {
-    document.getElementsByClassName("create-quiz-button")[0].addEventListener("click", function () {
-        document.getElementsByClassName("question-dropdown")[0].innerHTML = "<select id=\"list\" class=\"btn btn-warning \">\n" +
+    document.getElementsByClassName("save-quiz-button")[0].addEventListener("click", function () {
+        var quizName = document.getElementsByClassName("quiz-name")[0].value;
+        document.getElementsByClassName("question-dropdown")[0].innerHTML = "<h2>"+  quizName +"</h2><select id=\"list\" class=\"btn btn-warning \">\n" +
             "    <option value=\"QA\">Question - Answer</option>\n" +
             "    <option value=\"CTP\">Connect to Pictures</option>\n" +
             "    <option value=\"AN\">Anagram</option>\n" +
             "    <option value=\"AC\">Association Circle</option>\n" +
             "</select>"
-
+        //save quiz to db
         document.getElementById("list").addEventListener("click", dropdownEventListeners );
     });
 }
 
 function dropdownEventListeners() {
     var e = document.getElementById("list");
-    document.getElementsByClassName("question-form")[0].innerHTML = questionform[e.options[e.selectedIndex].value]
+    document.getElementsByClassName("question-form")[0].innerHTML = questionform[e.options[e.selectedIndex].value] +
+            "    <button id=\"left-arrow\" type=\"button\" class = \"btn btn-warning \"> <i class=\"far fa-3x fa-caret-square-left\"></i> </button> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
+            "    <button id=\"right-arrow\" type=\"button\" class = \"btn btn-warning \"> <i class=\"far fa-3x fa-caret-square-right\"></i> </button><br>\n"
+
 
 }
 
--- src/main/resources/static/js/questionforms.js
+++ src/main/resources/static/js/questionforms.js
@@ -1,10 +1,7 @@
 var questionform = {
-    "QA" : "<form action=\"/save-question\" method=\"post\">\n" +
+    "QA" :
     "    Question: <input type=\"text\" name=\"question\"><br>\n" +
-    "    Answer: <input type=\"text\" name=\"answer\"><br>\n<br><br>" +
-    "    <button id=\"left-arrow\" type=\"button\" class = \"btn btn-warning \"> <i class=\"far fa-3x fa-caret-square-left\"></i> </button> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
-    "    <button id=\"right-arrow\" type=\"button\" class = \"btn btn-warning \"> <i class=\"far fa-3x fa-caret-square-right\"></i> </button><br>\n" +
-    "</form>",
+    "    Answer: <input type=\"text\" name=\"answer\"><br>\n<br><br>",
 
 
     "CTP" : "connectToPictures",
